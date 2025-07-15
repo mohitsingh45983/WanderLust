18e689e68cf09024475da849accc2aa0d9f77e87
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-07-15 16:42:46 +0530

    commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require('./model/listing.js')
 const path = require('path')
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 main()
   .then(() => {
@@ -23,6 +24,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.engine('ejs',ejsMate);
 
 app.get('/', (req, res) => {
   res.send('working')
