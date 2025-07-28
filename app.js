c44babb1ce4c6f4103d1d565eb00ee651c6598ec
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-07-28 23:48:55 +0530

    Review Section Done

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.get(
   '/listings/:id',
   wrapAsync(async (req, res) => {
     let { id } = req.params
-    const showListing = await Listing.findById(id)
+    const showListing = await Listing.findById(id).populate('reviews')
     res.render('listings/show.ejs', { showListing })
   })
 )
@@ -123,14 +123,29 @@ app.delete(
 
 //Reviews
 //post Route
-app.post('/listings/:id/reviews', validateReview, wrapAsync( async (req, res) => {
-  let listing = await Listing.findById(req.params.id)
-  let newReview = new Review(req.body.review)
-  listing.reviews.push(newReview)
-  await newReview.save()
-  await listing.save()
-  res.redirect(`/listings/${listing._id}`)
-}));
+app.post(
+  '/listings/:id/reviews',
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id)
+    let newReview = new Review(req.body.review)
+    listing.reviews.push(newReview)
+    await newReview.save()
+    await listing.save()
+    res.redirect(`/listings/${listing._id}`)
+  })
+)
+
+//delete review route
+app.delete(
+  '/listings/:id/reviews/:reviewId',
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id,{$pull: {reviews : reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 // Catch all unmatched routes
 app.use((req, res, next) => {
--- model/listing.js
+++ model/listing.js
@@ -1,35 +1,43 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Review = require('./review.js')
 
-const listingSchema =  new Schema({
-    title:
-    {
-        type: String,
-        requires: true,
-    },
-    description:
-    {
-        type: String,
-    },
-   image: {
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    requires: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
     filename: String,
     url: {
-        type: String,
-        default: "https://thewowstyle.com/wp-content/uploads/2015/01/nature-images..jpg",
-        set: (v) =>
-        v === " " ? "https://thewowstyle.com/wp-content/uploads/2015/01/nature-images..jpg" : v,
-    }
+      type: String,
+      default:
+        'https://thewowstyle.com/wp-content/uploads/2015/01/nature-images..jpg',
+      set: (v) =>
+        v === ' '
+          ? 'https://thewowstyle.com/wp-content/uploads/2015/01/nature-images..jpg'
+          : v,
     },
-    price: Number,
-    location: String,
-    country: String,
-    reviews: [
-        {
-           type: Schema.Types.ObjectId,
-           ref : "Review",
-        },
-    ],
-});
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
+})
 
-const Listing = mongoose.model("Listing",listingSchema);
-module.exports = Listing;
+listingSchema.post('findOneAndDelete', async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } })
+  }
+})
+
+const Listing = mongoose.model('Listing', listingSchema)
+module.exports = Listing
