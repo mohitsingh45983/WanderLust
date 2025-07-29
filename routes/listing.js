5f67bd0717448bb9f8bf04f4b9529437bec8d610
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-07-29 22:55:00 +0530

    Resolving errors related to listing edit and add

--- routes/listing.js
+++ routes/listing.js
@@ -9,7 +9,7 @@ const Listing = require('../model/listing.js')
 const validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body)
   if (error) {
-    let errMsg = error.datails.map((el) => el.message).join(',')
+    let errMsg = error.details.map((el) => el.message).join(',')
     throw new ExpressError(400, errMsg)
   } else next()
 }
--- routes/review.js
+++ routes/review.js
@@ -10,7 +10,7 @@ const Listing = require('../model/listing.js')
 const validateReview = (req, res, next) => {
   let { error } = reviewSchema.validate(req.body)
   if (error) {
-    let errMsg = error.datails.map((el) => el.message).join(',')
+    let errMsg = error.details.map((el) => el.message).join(',')
     throw new ExpressError(400, errMsg)
   } else next()
 }
