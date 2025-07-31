ba82e05226e2835de1363abbd2fe4b9de62c9c07
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-07-31 18:58:16 +0530

    Authorization for listing done

--- routes/listing.js
+++ routes/listing.js
@@ -1,19 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const wrapAsync = require('../utils/wrapAsync.js')
-const ExpressError = require('../utils/ExpressError.js')
-const { listingSchema } = require('../schema.js')
 const Listing = require('../model/listing.js')
-const { isLoggedIn } = require('../middleware.js')
+const { isLoggedIn , isOwner , validateListing } = require('../middleware.js')
 
-// server side validation of forms
-const validateListing = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body)
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(',')
-    throw new ExpressError(400, errMsg)
-  } else next()
-}
+
 
 //Index Route
 router.get(
@@ -49,7 +40,7 @@ router.get(
   '/:id',
   wrapAsync(async (req, res) => {
     let { id } = req.params
-    const showListing = await Listing.findById(id).populate('reviews')
+    const showListing = await Listing.findById(id).populate('reviews').populate('owner')
     if (!showListing) {
       req.flash('error', 'Listing you requested for does not exist')
       return res.redirect('/listings')
@@ -62,6 +53,7 @@ router.get(
 router.get(
   '/:id/edit',
   isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params
     const listing = await Listing.findById(id)
@@ -77,11 +69,13 @@ router.get(
 router.put(
   '/:id',
   isLoggedIn,
+  isOwner,
+  validateListing,
   wrapAsync(async (req, res) => {
-    let { id } = req.params
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing })
-    req.flash('success', 'Listing Updated!')
-    res.redirect(`/listings/${id}`)
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash('success', 'Listing Updated!');
+    res.redirect(`/listings/${id}`);
   })
 )
 
@@ -89,6 +83,7 @@ router.put(
 router.delete(
   '/:id',
   isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params
     await Listing.findByIdAndDelete(id)
@@ -97,4 +92,4 @@ router.delete(
   })
 )
 
-module.exports = router
+module.exports = router;
--- routes/review.js
+++ routes/review.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 const wrapAsync = require('../utils/wrapAsync.js')
-const ExpressError = require('../utils/ExpressError.js')
-const { reviewSchema } = require('../schema.js')
 const Review = require('../model/review.js')
 const Listing = require('../model/listing.js')
-
-// server side validation of forms
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body)
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(',')
-    throw new ExpressError(400, errMsg)
-  } else next()
-}
+const { isLoggedIn , isOwner , validateReview } = require('../middleware.js')
 
 //post Route
 router.post(
