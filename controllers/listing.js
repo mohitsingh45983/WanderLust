9bdcf2a53e01e5232db8fe667d351c0bf8cd8273
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-04 22:36:07 +0530

    using GeoJson for storing coordinates and mapbox for map

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,4 +1,7 @@
 const Listing = require('../model/listing')
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding')
+const mapToken = process.env.MAP_TOKEN
+const geocodingClient = mbxGeocoding({ accessToken: mapToken })
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({})
@@ -10,12 +13,20 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createListing = async (req, res) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    })
+    .send()
+
   let url = req.file.path
   let filename = req.file.filename
   let listing = req.body.listing
   let newListing = new Listing(listing)
   newListing.owner = req.user._id
   newListing.image = { url, filename }
+  newListing.geometry = response.body.features[0].geometry
   await newListing.save()
   req.flash('success', 'New Listing Created!')
   res.redirect('/listings')
@@ -41,17 +52,17 @@ module.exports.editListing = async (req, res) => {
     return res.redirect('/listings')
   }
 
-  let originalImageUrl = listing.image.url;
-  originalImageUrl = originalImageUrl.replace("/upload","/upload/h_250,w_300");
+  let originalImageUrl = listing.image.url
+  originalImageUrl = originalImageUrl.replace('/upload', '/upload/h_250,w_300')
   console.log(originalImageUrl)
-  res.render('listings/edit.ejs', { listing , originalImageUrl})
+  res.render('listings/edit.ejs', { listing, originalImageUrl })
 }
 
 module.exports.updateListing = async (req, res) => {
   let { id } = req.params
   let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing })
 
-  if (typeof req.file != "undefined") {
+  if (typeof req.file != 'undefined') {
     let url = req.file.path
     let filename = req.file.filename
     listing.image = { url, filename }
